716b795e23564dd6782e2fe53fd07efbf6303e79
Christoph Baudson <christoph.baudson@rewe-digital.com>
2019-12-02 20:40:07 +0100

    day 2 revisited

--- src/main/kotlin/advent_of_code/Day02.kt
+++ src/main/kotlin/advent_of_code/Day02.kt
@@ -7,11 +7,12 @@ object Day02 {
         .getResource("day02_gravity_assist_program.txt")!!
         .readText()
 
+
     fun partOne(): IntCode {
         val memory = memory(input)
         memory[1] = 12
         memory[2] = 2
-        return IntCode(memory).execute()
+        return IntCode(memory).run()
     }
 
     fun partTwo(): Int? {
@@ -20,7 +21,7 @@ object Day02 {
                 val memory = memory(input)
                 memory[1] = noun
                 memory[2] = verb
-                if (IntCode(memory).execute().memory[0] == 19_690_720) {
+                if (IntCode(memory).run().memory[0] == 19_690_720) {
                     return 100 * noun + verb
                 }
             }
@@ -29,22 +30,31 @@ object Day02 {
     }
 }
 
+typealias Address = Int
+typealias Memory = MutableMap<Address, Int>
+
+fun memory(input: String): Memory {
+    val values = input.split(",").mapIndexed { address, value -> address to value.toInt() }
+    return mutableMapOf(*values.toTypedArray())
+}
+
+
 enum class Opcode(val value: Int) {
     ADDITION(1),
     MULTIPLICATION(2),
     TERMINATION(99);
 
     companion object {
-        private val map = values().associateBy(Opcode::value)
-        fun from(value: Int): Opcode = map.getOrDefault(value, TERMINATION)
+        private val opcodes = values().associateBy(Opcode::value)
+        fun from(value: Int): Opcode = opcodes.getOrDefault(value, TERMINATION)
     }
 }
 
-typealias Parameter = Int
 
 sealed class Instruction {
     abstract val opcode: Opcode
 }
+typealias Parameter = Int
 
 class Addition(val p1: Parameter, val p2: Parameter, val p3: Parameter) : Instruction() {
     override val opcode = ADDITION
@@ -58,25 +68,27 @@ class Termination : Instruction() {
     override val opcode = TERMINATION
 }
 
-typealias Address = Int
+
 typealias InstructionPointer = Address
 
 data class IntCode(val memory: Memory) {
 
-    fun execute(): IntCode = execute(next()).also { i += 4 }?.execute() ?: this
+    fun run(): IntCode = process(next())
+        .also { i += 4 }
+        ?.run() ?: this
 
     private var i: InstructionPointer = 0
 
     private fun memory(address: Address) = memory[address]!!
 
-    private fun execute(instruction: Instruction) = instruction.run {
+    private fun process(instruction: Instruction) = instruction.run {
         when (this) {
             is Addition -> {
-                memory[p3] = memory(p1).plus(memory(p2))
+                memory[p3] = memory(p1) + memory(p2)
                 this@IntCode
             }
             is Multiplication -> {
-                memory[p3] = memory(p1).times(memory(p2))
+                memory[p3] = memory(p1) * memory(p2)
                 this@IntCode
             }
             is Termination -> null
@@ -98,9 +110,3 @@ data class IntCode(val memory: Memory) {
     }
 }
 
-typealias Memory = MutableMap<Int, Int>
-
-fun memory(input: String): Memory {
-    val addresses = input.split(",").mapIndexed { index, value -> index to value.toInt() }
-    return mutableMapOf(*addresses.toTypedArray())
-}
--- src/test/kotlin/advent_of_code/Day02Test.kt
+++ src/test/kotlin/advent_of_code/Day02Test.kt
@@ -6,16 +6,16 @@ import kotlin.test.assertEquals
 class Day02Test {
     @Test
     fun `part one`() {
-        assertEquals(IntCode(memory("99")), IntCode(memory("99")).execute())
-        assertEquals(IntCode(memory("2,0,0,0,99")), IntCode(memory("1,0,0,0,99")).execute())
-        assertEquals(IntCode(memory("2,3,0,6,99")), IntCode(memory("2,3,0,3,99")).execute())
-        assertEquals(IntCode(memory("2,4,4,5,99,9801")), IntCode(memory("2,4,4,5,99,0")).execute())
-        assertEquals(IntCode(memory("30,1,1,4,2,5,6,0,99")), IntCode(memory("1,1,1,4,99,5,6,0,99")).execute())
+        assertEquals(IntCode(memory("99")), IntCode(memory("99")).run())
+        assertEquals(IntCode(memory("2,0,0,0,99")), IntCode(memory("1,0,0,0,99")).run())
+        assertEquals(IntCode(memory("2,3,0,6,99")), IntCode(memory("2,3,0,3,99")).run())
+        assertEquals(IntCode(memory("2,4,4,5,99,9801")), IntCode(memory("2,4,4,5,99,0")).run())
+        assertEquals(IntCode(memory("30,1,1,4,2,5,6,0,99")), IntCode(memory("1,1,1,4,99,5,6,0,99")).run())
 
         val memory = memory(Day02.input)
         memory[1] = 12
         memory[2] = 2
-        assertEquals(3716250, IntCode(memory).execute().memory[0])
+        assertEquals(3716250, IntCode(memory).run().memory[0])
     }
 
     @Test
