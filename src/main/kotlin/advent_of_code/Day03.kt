03916a9750fb434e3b2016b0fce28b432fd7cab1
Christoph Baudson <christoph.baudson@rewe-digital.com>
2019-12-03 20:22:06 +0100

    Day3 clean up

--- src/main/kotlin/advent_of_code/Day03.kt
+++ src/main/kotlin/advent_of_code/Day03.kt
@@ -3,14 +3,13 @@ package advent_of_code
 import kotlin.math.abs
 
 object Day03 {
-    val cables = javaClass.classLoader
+    val cablePair = javaClass.classLoader
         .getResource("day03_cables.txt")!!
-        .readText().lines().map { cable(it) }
+        .readText().lines().map { cable(it) }.zipWithNext().first()
 
-    fun partOne(): Int = cables[0].distanceToClosestIntersectionToPort(cables[1])
+    fun partOne(): Int = cablePair.distanceToClosestIntersection()
 
-    fun partTwo(): Int = cables[0].stepsToClosestIntersection(cables[1])
-
+    fun partTwo(): Int = cablePair.stepsToClosestIntersection()
 }
 
 enum class Direction(val input: Char) {
@@ -20,59 +19,68 @@ enum class Direction(val input: Char) {
     UP('U');
 
     companion object {
-        private val opcodes = Direction.values().associateBy(Direction::input)
-        fun from(value: Char): Direction? = opcodes.get(value)
+        private val directions = values().associateBy(Direction::input)
+        fun from(input: Char): Direction? = directions[input]
     }
 }
 
 data class CableSegment(val direction: Direction, val distance: Int) {
-
-}
-typealias Cable = List<Pair<Int, Int>>
-
-fun cable(input: String): Cable = input.split(",").map { segment ->
-    val direction = Direction.from(segment[0])!!
-    val distance = segment.substring(1).toInt()
-    CableSegment(direction, distance)
-}.run {
-    cable(this)
+    companion object {
+        fun create(segment: String): CableSegment = CableSegment(
+            Direction.from(segment[0])!!,
+            segment.substring(1).toInt()
+        )
+    }
 }
 
 
-fun cable(cablesSegments: List<CableSegment>): Cable {
-    val cable = mutableListOf(0 to 0) // port
-    var pos = cable[0]
-    cablesSegments.forEach { segment ->
-        val dir = when (segment.direction) {
+fun cable(line: String): Cable = line.split(",")
+    .map { CableSegment.create(it) }
+    .run { cable(this) }
+
+fun cable(segments: List<CableSegment>): Cable {
+    var point: Port = 0 to 0
+    val cable = mutableListOf<Point>(point)
+    segments.forEach { segment ->
+        val step: Point = when (segment.direction) {
             Direction.RIGHT -> (1 to 0)
             Direction.DOWN -> (0 to -1)
             Direction.LEFT -> (-1 to 0)
             Direction.UP -> (0 to 1)
         }
         repeat(segment.distance) {
-            pos = (pos.first + dir.first to pos.second + dir.second)
-            val next = pos
-            cable.add(next)
+            point += step
+            cable.add(point)
         }
     }
     return cable.toList()
 }
 
-fun Cable.distanceToPort(point: Pair<Int, Int>): Int =
-    abs(point.first - this[0].first) + abs(point.second - this[0].second)
+typealias Point = Pair<Int, Int>
 
-fun Cable.distanceToClosestIntersectionToPort(cable: Cable): Int = distanceToPort(closestIntersectionToPort(cable))
+operator fun Point.plus(p: Point) = (first + p.first to second + p.second)
 
-fun Cable.closestIntersectionToPort(cable: Cable): Pair<Int, Int> =
-    intersections(cable).reduce { acc, pair ->
-        if (this@closestIntersectionToPort.distanceToPort(pair) < this@closestIntersectionToPort.distanceToPort(acc)) {
-            pair
-        } else acc
-    }
+infix fun Point.distanceTo(p: Point) = abs(first - p.first) + abs(second - p.second)
 
-fun Cable.intersections(cable: Cable): List<Pair<Int, Int>> = toSet().intersect(cable.toSet()).minus(this[0]).toList()
 
-fun List<Cable>.stepsToIntersection(intersection: Pair<Int, Int>): Int = sumBy { it.indexOf(intersection) }
+typealias Cable = List<Point>
+typealias Port = Point
 
-fun Cable.stepsToClosestIntersection(cable: Cable): Int = intersections(cable).map { listOf(this, cable).stepsToIntersection(it)}.min() ?: 0
+fun Cable.port(): Port = this[0]
 
+
+typealias CablePair = Pair<Cable, Cable>
+typealias Intersection = Point
+
+
+fun CablePair.distanceToClosestIntersection(): Int = first.port() distanceTo closestIntersection()
+
+fun CablePair.closestIntersection(): Intersection = intersections().reduce { acc, intersection ->
+    if (first.port() distanceTo intersection < first.port() distanceTo acc) intersection else acc
+}
+
+fun CablePair.intersections(): List<Intersection> = first.toSet().intersect(second.toSet()).minus(first.port()).toList()
+
+fun CablePair.stepsTo(intersection: Intersection): Int = toList().sumBy { cable -> cable.indexOf(intersection) }
+
+fun CablePair.stepsToClosestIntersection(): Int = intersections().map { stepsTo(it) }.min() ?: 0
