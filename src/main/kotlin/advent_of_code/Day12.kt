a9c1a6807c3316b04eb7a3724c001ba39b906840
Christoph Baudson <christoph.baudson@rewe-digital.com>
2019-12-12 23:14:42 +0100

    Day 12 part 2

--- src/main/kotlin/advent_of_code/Day12.kt
+++ src/main/kotlin/advent_of_code/Day12.kt
@@ -1,6 +1,7 @@
 package advent_of_code
 
 import kotlin.math.abs
+import kotlin.math.sign
 
 fun main(args: Array<String>) {
     println(Day12.partOne())
@@ -34,37 +35,49 @@ object Day12 {
             if (yPeriod == 0L && step.check(Vector3::y, moons)) yPeriod = counter
             if (zPeriod == 0L && step.check(Vector3::z, moons)) zPeriod = counter
         }
-        return lcm(xPeriod, lcm(yPeriod, zPeriod))
+        return lcm(xPeriod, yPeriod, zPeriod)
 
     }
 }
-fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
-fun lcm(a: Long, b: Long): Long = a / gcd(a, b) * b
+
+fun lcm(a: Long, b: Long, vararg numbers: Long): Long = when {
+    numbers.isEmpty() -> abs(a * b) / gcd(a, b)
+    else -> lcm(lcm(a, b), numbers.first(), *numbers.drop(1).toLongArray())
+}
+
+fun gcd(a: Long, b: Long, vararg numbers: Long): Long = when {
+    numbers.isEmpty() -> if (b == 0L) a else gcd(b, a % b)
+    else -> gcd(gcd(a, b), numbers.first(), *numbers.drop(1).toLongArray())
+}
 
 typealias MoonsInput = String
-fun MoonsInput.load() = lines().map { it
-    .replace("<", "")
-    .replace(">", "")
-    .replace("x=", "")
-    .replace("y=", "")
-    .replace("z=", "")
-    .split(", ").map { it.toInt() }
+
+fun MoonsInput.load() = lines().map {
+    it
+        .replace("<", "")
+        .replace(">", "")
+        .replace("x=", "")
+        .replace("y=", "")
+        .replace("z=", "")
+        .split(", ").map { it.toInt() }
 }.map { (x, y, z) -> Moon(Pos(x, y, z), Velocity(0, 0, 0)) }
 
 typealias AxisVelocity = Int
+typealias Energy = Int
+typealias Moons = List<Moon>
+typealias Pos = Vector3
+typealias Velocity = Vector3
 typealias Z = Int
 
 data class Vector3(val x: X, val y: Y, val z: Z) {
-    operator fun plus(vector3: Vector3) : Vector3 = Vector3(x + vector3.x, y + vector3.y, z + vector3.z)
+    operator fun plus(vector3: Vector3): Vector3 = Vector3(x + vector3.x, y + vector3.y, z + vector3.z)
 }
 
-typealias Pos = Vector3
-typealias Velocity = Vector3
+
 
 data class Moon(val pos: Pos, val velocity: Velocity) {
-    operator fun plus(v: Velocity): Moon {
-        val velocityAfterGravity = velocity + v
-        return Moon(pos + velocityAfterGravity, velocityAfterGravity)
+    operator fun plus(v: Velocity): Moon = (velocity + v).run {
+        Moon(pos + this, this)
     }
 
     fun gravitate(other: Moon): Moon = copy(
@@ -77,22 +90,15 @@ data class Moon(val pos: Pos, val velocity: Velocity) {
 
     fun applyVelocity(): Moon = copy(pos = pos + velocity)
 
-    private inline fun gravitateAxis(other: Moon, axis: Vector3.() -> Int): Int = when {
-        pos.axis() > other.pos.axis() -> velocity.axis() - 1
-        pos.axis() < other.pos.axis() -> velocity.axis() + 1
-        else -> velocity.axis()
-    }
-
+    private inline fun gravitateAxis(other: Moon, axis: Vector3.() -> Int): Int = velocity.axis() + (other.pos.axis() - pos.axis()).sign
 
     fun totalEnergy(): Energy = potentialEnergy() * kineticEnergy()
-
     private fun potentialEnergy(): Energy = pos.energy()
-
     private fun kineticEnergy(): Energy = velocity.energy()
 }
 
-typealias Energy = Int
-typealias Moons = List<Moon>
+
+
 fun Moons.simulateGravity(): Moons {
     val pairs = (0 until size).map { x -> (0 until size).map { y -> get(x) to get(y) } }
     return pairs.map { pairsForMoon ->
@@ -109,27 +115,23 @@ fun Moons.simulateGravity(): Moons {
 fun Moons.totalEnergy() = map { it.totalEnergy() }.sum()
 
 typealias MoonPair = Pair<Moon, Moon>
-fun MoonPair.simulateGravity() : Velocity {
+
+fun MoonPair.simulateGravity(): Velocity {
     return (first.pos to second.pos).gravity()
 }
 
 typealias VelocityPair = Pair<Velocity, Velocity>
 typealias AxisVelocityPair = Pair<AxisVelocity, AxisVelocity>
-fun VelocityPair.gravity() : Velocity = Velocity(
+
+fun VelocityPair.gravity(): Velocity = Velocity(
     (first.x to second.x).gravity(),
     (first.y to second.y).gravity(),
     (first.z to second.z).gravity()
 )
 
-fun AxisVelocityPair.gravity(): AxisVelocity = when {
-    first < second -> 1
-    first > second -> -1
-    else -> 0
-}
+fun AxisVelocityPair.gravity(): AxisVelocity = (second - first).sign
 
 fun Vector3.energy() = abs(x) + abs(y) + abs(z)
-
-
 
 private fun List<Moon>.step(): List<Moon> = mapIndexed { index, moon ->
     indices.filterNot { it == index }
@@ -138,10 +140,9 @@ private fun List<Moon>.step(): List<Moon> = mapIndexed { index, moon ->
         .applyVelocity()
 }
 
-private inline fun List<Moon>.check(axis: Vector3.() -> Int, moons:Moons): Boolean =
+private inline fun List<Moon>.check(axis: Vector3.() -> Int, moons: Moons): Boolean =
     indices.all { idx -> matches(idx, axis, moons) }
 
-private inline fun List<Moon>.matches(index: Int, axis: Vector3.() -> Int, moons:Moons): Boolean =
-    this[index].pos.axis() == moons[index].pos.axis() &&
-            this[index].velocity.axis() == moons[index].velocity.axis()
+private inline fun List<Moon>.matches(index: Int, axis: Vector3.() -> Int, moons: Moons): Boolean =
+    this[index].pos.axis() == moons[index].pos.axis() && this[index].velocity.axis() == moons[index].velocity.axis()
 
