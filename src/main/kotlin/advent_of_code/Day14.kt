2b2516b0bb66cdc5904ccf20d6403c9424ebf979
Christoph Baudson <christoph.baudson@rewe-digital.com>
2019-12-13 17:31:08 +0100

    day 14 prep

---
+++ src/main/kotlin/advent_of_code/Day14.kt
@@ -0,0 +1,11 @@
+package advent_of_code
+
+fun main(args: Array<String>) {
+    println(Day14.partOne())
+    println(Day14.partTwo())
+}
+
+object Day14 {
+    fun partOne(): Int = 0
+    fun partTwo(): Int = 0
+}
---
+++ src/test/kotlin/advent_of_code/Day14Test.kt
@@ -0,0 +1,16 @@
+package advent_of_code
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day14Test {
+    @Test
+    fun `part one`() {
+        assertEquals(0, Day14.partOne())
+    }
+
+    @Test
+    fun `part two`() {
+        assertEquals(0, Day14.partTwo())
+    }
+}
