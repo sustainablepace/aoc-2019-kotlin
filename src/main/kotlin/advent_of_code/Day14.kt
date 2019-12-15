9af1a189e73c0884203b8065e000bc95370af334
Christoph Baudson <christoph.baudson@rewe-digital.com>
2019-12-15 10:04:43 +0100

    day 14 wip

--- src/main/kotlin/advent_of_code/Day14.kt
+++ src/main/kotlin/advent_of_code/Day14.kt
@@ -1,11 +1,100 @@
 package advent_of_code
 
+import kotlin.math.ceil
+
 fun main(args: Array<String>) {
     println(Day14.partOne())
     println(Day14.partTwo())
 }
 
 object Day14 {
-    fun partOne(): Int = 0
-    fun partTwo(): Int = 0
+    private val reactionsList = javaClass.classLoader
+        .getResource("day14_reaction.txt")!!
+        .readText()
+
+    private val reactions = reactionsList.parse()
+
+    fun partOne(): Long = reactions.minOreForFuel(1)
+
+    fun partTwo(): Long = reactions.maxFuelForOre(1_000_000_000_000)
 }
+
+typealias Reactions = Set<Reaction>
+data class Reaction(val input: Ingredients, val output: Ingredient)
+data class Ingredient(val amount: Amount, val chemical: Chemical)
+typealias Ingredients = List<Ingredient>
+typealias Chemical = String
+typealias Amount = Long
+
+fun Ingredients.containsOtherThanOre() = any { it.chemical != "ORE" }
+fun Ingredients.aggregate() = groupBy {
+    it.chemical
+}.map { group ->
+    Ingredient(group.value.map { it.amount }.sum(), group.key)
+}
+
+fun Reactions.isDeferredFor(chemical: Chemical) =
+    any { reaction -> reaction.input.any { ingredient -> ingredient.chemical == chemical } }
+
+fun Reactions.into(chemical: Chemical) = filter { it.output.chemical == chemical }
+
+fun Reactions.substitute(ingredient: Ingredient): Ingredients =
+    into(ingredient.chemical).flatMap { reaction ->
+        reaction.input.map {
+            Ingredient(
+                ceil(ingredient.amount.toFloat() / reaction.output.amount).toInt() * it.amount,
+                it.chemical
+            )
+
+        }
+    }
+
+
+fun Ingredients.substitute(reactions: Reactions) = flatMap { ingredient ->
+    if (reactions.isDeferredFor(ingredient.chemical)) {
+        listOf(ingredient)
+    } else {
+        reactions.substitute(ingredient)
+    }
+}
+
+fun Reactions.maxFuelForOre(numFuel: Amount): Amount {
+    return 0
+}
+
+fun Reactions.minOreForFuel(numFuel: Amount): Amount {
+    var availableReactions = toMutableSet()
+    var requiredIngredients = listOf(Ingredient(numFuel, "FUEL"))
+
+    while (requiredIngredients.containsOtherThanOre()) {
+        val requiredReactions = availableReactions.filter { reaction ->
+            requiredIngredients.any { ingredient -> ingredient.chemical == reaction.output.chemical }
+        }.toSet()
+        val newRequiredIngredients = requiredIngredients.substitute(availableReactions).aggregate()
+
+        val obsoleteReactions = requiredReactions.filter { reaction ->
+            newRequiredIngredients.none { it.chemical == reaction.output.chemical } &&
+                    availableReactions.none { it.input.any { it.chemical == reaction.output.chemical } }
+        }.toSet()
+        availableReactions = availableReactions.minus(obsoleteReactions).toMutableSet()
+
+        requiredIngredients = newRequiredIngredients
+    }
+    return requiredIngredients.map { it.amount }.sum()
+}
+
+
+typealias ReactionsList = String
+
+fun ReactionsList.parse(): Reactions = lines().map { line ->
+    val (input, output) = line.split(" => ")
+    val left = input.split(", ").map {
+        val (amount, name) = it.split(" ")
+        Ingredient(amount.toLong(), name)
+    }
+    val right = output.run {
+        val (amount, name) = split(" ")
+        Ingredient(amount.toLong(), name)
+    }
+    Reaction(left, right)
+}.toSet()
--- src/test/kotlin/advent_of_code/Day14Test.kt
+++ src/test/kotlin/advent_of_code/Day14Test.kt
@@ -4,9 +4,90 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day14Test {
+
+    @Test
+    fun `part one - example 1`() {
+        val part1example1 = javaClass.classLoader
+            .getResource("day14_part1_example1.txt")!!
+            .readText()
+
+        val reactions = part1example1.parse()
+        assertEquals(31, reactions.minOreForFuel(1))
+    }
+
+    @Test
+    fun `part one - example 2`() {
+        val part1example1 = javaClass.classLoader
+            .getResource("day14_part1_example2.txt")!!
+            .readText()
+
+        val reactions = part1example1.parse()
+        assertEquals(165, reactions.minOreForFuel(1))
+    }
+
+    @Test
+    fun `part one - example 3`() {
+        val part1example1 = javaClass.classLoader
+            .getResource("day14_part1_example3.txt")!!
+            .readText()
+
+        val reactions = part1example1.parse()
+        assertEquals(13312, reactions.minOreForFuel(1))
+    }
+
+    @Test
+    fun `part one - example 4`() {
+        val part1example1 = javaClass.classLoader
+            .getResource("day14_part1_example4.txt")!!
+            .readText()
+
+        val reactions = part1example1.parse()
+        assertEquals(180697, reactions.minOreForFuel(1))
+    }
+
+    @Test
+    fun `part one - example 5`() {
+        val part1example1 = javaClass.classLoader
+            .getResource("day14_part1_example5.txt")!!
+            .readText()
+
+        val reactions = part1example1.parse()
+        assertEquals(2210736, reactions.minOreForFuel(1))
+    }
+
     @Test
     fun `part one`() {
-        assertEquals(0, Day14.partOne())
+        assertEquals(168046, Day14.partOne())
+    }
+
+    @Test
+    fun `part two - example 3`() {
+        val part1example1 = javaClass.classLoader
+            .getResource("day14_part1_example3.txt")!!
+            .readText()
+
+        val reactions = part1example1.parse()
+        assertEquals(82892753, reactions.maxFuelForOre(1_000_000_000_000))
+    }
+
+    @Test
+    fun `part two - example 4`() {
+        val part1example1 = javaClass.classLoader
+            .getResource("day14_part1_example4.txt")!!
+            .readText()
+
+        val reactions = part1example1.parse()
+        assertEquals(5586022, reactions.maxFuelForOre(1_000_000_000_000))
+    }
+
+    @Test
+    fun `part two - example 5`() {
+        val part1example1 = javaClass.classLoader
+            .getResource("day14_part1_example5.txt")!!
+            .readText()
+
+        val reactions = part1example1.parse()
+        assertEquals(460664, reactions.maxFuelForOre(1_000_000_000_000))
     }
 
     @Test
